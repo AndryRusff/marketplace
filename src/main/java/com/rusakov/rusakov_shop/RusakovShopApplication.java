c56653edb319dba33ec2feeea04a5b54c6966122
Андрей Русаков <arusakov@rzdit.ru>
2023-01-06 22:19:30 +0300

    Initial commit

---
+++ src/main/java/com/rusakov/rusakov_shop/RusakovShopApplication.java
@@ -0,0 +1,13 @@
+package com.rusakov.rusakov_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RusakovShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RusakovShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rusakov/rusakov_shop/RusakovShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rusakov.rusakov_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RusakovShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
